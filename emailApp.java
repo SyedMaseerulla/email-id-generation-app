df71e613ead7bbc834584d27fcf799d9147afcd4
someone <someone@someplace.com>
2018-10-30 21:14:24 +0530

    The emailApp class is added which can create objects

---
+++ emailApp.java
@@ -0,0 +1,10 @@
+package email_app;
+
+public class emailApp {
+	public static void main(String args[]){
+		email em1 = new email("James", "Bond");
+		
+		System.out.println(em1.showinfo());
+	}
+
+}
